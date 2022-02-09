d7b7a5f028c6d66b1d1cd6fb8bfd01bdf1b737b9
晓斌 <xiaobin@xiaobindeair.lan>
2022-02-08 21:35:49 -0800

    fix extract text

--- crawler/crawlParser.py
+++ crawler/crawlParser.py
@@ -1,23 +1,28 @@
 import tokenizer
 from bs4 import BeautifulSoup
 from bs4.element import Comment
+import re
+
 
 def filter_nontext(element):
     if isinstance(element, Comment):
         return False
     if element.parent.name in ['[document]', 'script', 'meta', 'head', 'html', 'input', 'style', 'noscript', 'canvas']:
         return False
+    if re.match(r'[\r\s\n]+', str(element)):
+        return False
     return True
+
 
 class CrawlParser:
     def __init__(self):
         pass
 
-    #need pre-parse html, arg is a html file
+    # need pre-parse html, arg is a html file
     def parse(self, file):
         t = tokenizer.tokenizer()
         soup = BeautifulSoup(file, features="html.parser")
-        #strip html header
+        # strip html header
         text = soup.find_all(text=True)
         visible_text = filter(filter_nontext, text)
         for t in visible_text:
@@ -27,13 +32,13 @@ class CrawlParser:
         # print(word_dict)
         pass
 
-    #persistent results to files
+    # persistent results to files
     def persistent(self, tokens):
         pass
 
 
 if __name__ == "__main__":
-    f = open("../test/www.ics.uci.edu.html", "r")
+    f = open("../test/Donald Bren School of Information and Computer Sciences @ University of California, Irvine.html", "r")
     texts = f.read()
     c = CrawlParser()
     c.parse(texts)
